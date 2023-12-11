fc62b47308d8fcc4da599de3275b6bb40677a489
Emil Kondov <e.kondov96@gmail.com>
2023-12-11 23:04:16 +0200

    Update 1..py

--- Python Basics/Basic Data Types/1..py
+++ Python Basics/Basic Data Types/1..py
@@ -1,7 +1,6 @@
 # Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores.
 # Store them in a list and find the score of the runner-up.
 
-numbers_of_scores = int(input())
-scores = [int(x) for x in input().split()]
-
-print(scores[-1])
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
